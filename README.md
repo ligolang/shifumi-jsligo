66b756564379d1812a0eeee6f34f6ca2961a80ae
JulesGuesnon <jules.guesnon@hetic.net>
2022-12-20 17:42:29 +0100

    refactor: Normalization update

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Players can reveal their secret action and commit their secret choice.
 Once all players have revealed their secret action (for the current round), the result for this round is computed and the session goes to the next round. The result of the session is automatically computed once all rounds have been played.
 
 The smart contract provides an on-chian view for retrieving the session information:
+
 - the status of a session (Inplay, Winner, Draw)
 - winners for each round
 
@@ -23,10 +24,13 @@ The repository provides a Makefile for compiling/testing/deploying the smart con
 ### Compile Shifumi contract
 
 The repository provides a Makefile for compiling the smart contract Shifumi.
+
 ```
 make compile
 ```
+
 You can also override `make` parameters by running :
+
 ```sh
 make compile ligo_compiler=<LIGO_EXECUTABLE> protocol_opt="--protocol <PROTOCOL>"
 ```
@@ -36,6 +40,7 @@ It compiles the smart contract in TZ file and also in the JSON format
 ### Test Shifumi contract
 
 The repository provides a Makefile for testing the smart contract Shifumi.
+
 ```
 make test
 ```
@@ -43,13 +48,9 @@ make test
 ### Test Shifumi contract
 
 The repository provides a deployment script for deploying the smart contract Shifumi.
+
 ```
 make deploy
 ```
 
-It is based on a .env file that contains deployment information:
-```
-ADMIN_PK - private key
-ADMIN_ADDRESS - public key
-RPC - URL of the RPC node that will process the transaction
-```
+You have to rename `deploy/.env.dist` to `deploy/.env` and **fill the required variables** to be able to deploy.
--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,51 +1,104 @@
-import { InMemorySigner } from '@taquito/signer';
-import { TezosToolkit, MichelsonMap } from '@taquito/taquito';
-import shifumi from '../compiled/shifumi.json';
+import { InMemorySigner } from "@taquito/signer";
+import { MichelsonMap, TezosToolkit } from "@taquito/taquito";
 import { buf2hex } from "@taquito/utils";
+import chalk from "chalk";
+import { Spinner } from "cli-spinner";
+import * as dotenv from "dotenv";
+import code from "../compiled/shifumi.json";
 import metadata from "./metadata.json";
-import * as dotenv from 'dotenv'
 
-dotenv.config(({path:__dirname+'/.env'}))
+dotenv.config({ path: __dirname + "/.env" });
 
-const rpc = process.env.RPC; //"http://127.0.0.1:8732"
-const pk: string = process.env.ADMIN_PK || undefined;
-const Tezos = new TezosToolkit(rpc);
-const signer = new InMemorySigner(pk);
-Tezos.setProvider({ signer: signer })
+const missingEnvVarLog = (name: string) =>
+  console.log(
+    chalk.redBright`Missing ` +
+      chalk.red.bold.underline(name) +
+      chalk.redBright` env var. Please add it in ` +
+      chalk.red.bold.underline(`deploy/.env`)
+  );
 
-let shifumi_address = process.env.SHIFUMI_CONTRACT_ADDRESS || undefined;
+const makeSpinnerOperation = async <T>(
+  operation: Promise<T>,
+  {
+    loadingMessage,
+    endMessage,
+  }: {
+    loadingMessage: string;
+    endMessage: string;
+  }
+): Promise<T> => {
+  const spinner = new Spinner(loadingMessage);
+  spinner.start();
+  const result = await operation;
+  spinner.stop();
+  console.log("");
+  console.log(endMessage);
 
-async function orig() {
+  return result;
+};
 
-    let shifumi_store = {
-        'metadata': MichelsonMap.fromLiteral({
-            "": buf2hex(Buffer.from("tezos-storage:contents")),
-            contents: buf2hex(Buffer.from(JSON.stringify(metadata))),
-        }),
-        'next_session' : 0,
-        'sessions' : new MichelsonMap(),
-    }
+const rpcUrl = process.env.RPC_URL; //"http://127.0.0.1:8732"
+const pk = process.env.PK;
 
-    try {
-        // Originate an Random contract
-        if (shifumi_address === undefined) {
-            const shifumi_originated = await Tezos.contract.originate({
-                code: shifumi,
-                storage: shifumi_store,
-            })
-            console.log(`Waiting for SHIFUMI ${shifumi_originated.contractAddress} to be confirmed...`);
-            await shifumi_originated.confirmation(2);
-            console.log('confirmed SHIFUMI: ', shifumi_originated.contractAddress);
-            shifumi_address = shifumi_originated.contractAddress;              
-        }
-       
-        console.log("./tezos-client remember contract SHIFUMI", shifumi_address)
-        // console.log("tezos-client transfer 0 from ", admin, " to ", advisor_address, " --entrypoint \"executeAlgorithm\" --arg \"Unit\"")
+if (!pk && !rpcUrl) {
+  console.log(
+    chalk.redBright`Couldn't find env variables. Have you renamed ` +
+      chalk.red.bold.underline`deploy/.env.dist` +
+      chalk.redBright` to ` +
+      chalk.red.bold.underline(`deploy/.env`)
+  );
 
-    } catch (error: any) {
-        console.log(error)
-        return process.exit(1)
-    }
+  process.exit(-1);
 }
 
-orig();
+if (!pk) {
+  missingEnvVarLog("PK");
+  process.exit(-1);
+}
+
+if (!rpcUrl) {
+  missingEnvVarLog("RPC_URL");
+  process.exit(-1);
+}
+
+const Tezos = new TezosToolkit(rpcUrl);
+const signer = new InMemorySigner(pk);
+Tezos.setProvider({ signer: signer });
+
+async function deploy() {
+  let storage = {
+    metadata: MichelsonMap.fromLiteral({
+      "": buf2hex(Buffer.from("tezos-storage:contents")),
+      contents: buf2hex(Buffer.from(JSON.stringify(metadata))),
+    }),
+    next_session: 0,
+    sessions: new MichelsonMap(),
+  };
+
+  try {
+    const origination = await makeSpinnerOperation(
+      Tezos.contract.originate({
+        code,
+        storage,
+      }),
+      {
+        loadingMessage: chalk.yellowBright`Deploying contract`,
+        endMessage: chalk.green`Contract deployed!`,
+      }
+    );
+
+    await makeSpinnerOperation(origination.contract(), {
+      loadingMessage:
+        chalk.yellowBright`Waiting for contract to be confirmed at: ` +
+        chalk.yellow.bold(origination.contractAddress),
+      endMessage: chalk.green`Contract confirmed!`,
+    });
+  } catch (error: any) {
+    console.log("");
+    console.log(chalk.redBright`Error during deployment:`);
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+deploy();
